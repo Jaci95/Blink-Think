8ec3273566d6cd89fd56a731e9d3ed9a1dd5317d
PaulBankl <s2410238004@fhooe.at>
2026-03-24 20:03:11 +0100

    PauseMenu und su

--- Assets/Scripts/Paul/PauseManager.cs
+++ Assets/Scripts/Paul/PauseManager.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+
 public class PauseManager : MonoBehaviour
 {
     public GameObject PauseMenuUI;
-    private bool isPaused;
+    public static bool isPaused;
+
+    public InputAction cancel;
 
     public void Start()
     {
+        isPaused = false;
+        Time.timeScale = 1f;
         PauseMenuUI.SetActive(false);
+
+        cancel = InputSystem.actions.FindAction("UI/Cancel");
+
+        if (cancel == null)
+        {
+            Debug.Log("cancel action is null");
+            return;
+        }
+
+        cancel.Enable();
     }
 
     public void toMain()
@@ -16,31 +31,30 @@ public class PauseManager : MonoBehaviour
         Time.timeScale = 1f;
         SceneManager.LoadScene("StartMenu");
     }
+
     public void Update()
     {
-        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        if (cancel != null && cancel.WasPressedThisFrame())
         {
+
             if (isPaused)
-            {
                 ContinueGame();
-            }
             else
-            {
                 PauseGame();
-            }
+        }
     }
-    }
+
     public void PauseGame()
     {
         Time.timeScale = 0f;
         isPaused = true;
         PauseMenuUI.SetActive(true);
     }
+
     public void ContinueGame()
     {
         Time.timeScale = 1f;
         isPaused = false;
         PauseMenuUI.SetActive(false);
     }
-
 }
